84f69b7ab992ac6b9fafd1bea13f1904b5b37d8b
MiellyEllen <maryellen.bowman@gmail.com>
2019-06-18 19:14:03 -0400

    Final commit to 3.3

--- university/src/main/java/com/example/university/UniversityApplication.java
+++ university/src/main/java/com/example/university/UniversityApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UniversityApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UniversityApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityApplication.class, args);
+	}
 }
--- university/src/main/java/com/example/university/repo/DepartmentRepository.java
+++ university/src/main/java/com/example/university/repo/DepartmentRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * DataSource Management for the Departments at the University.
- * <p>
- * Created by maryellenbowman.
+ *
+ * Created by maryellenbowman
  */
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+
 }
