330a8c668cc1bed07baf02878541093a79e6adeb
MiellyEllen <maryellen.bowman@gmail.com>
2019-06-25 19:02:55 -0400

    Final commit to 3.3

--- university/src/main/java/com/example/university/repo/StudentRepository.java
+++ university/src/main/java/com/example/university/repo/StudentRepository.java
@@ -1,10 +1,7 @@
 package com.example.university.repo;
 
-import com.example.university.domain.Person;
 import com.example.university.domain.Student;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 /**
  * DataSource Management for the Students at the University.
@@ -12,19 +9,5 @@ import java.util.List;
  * Created by maryellenbowman
  */
 public interface StudentRepository extends CrudRepository<Student, Integer>{
-    //Simple Query Methods
-    List<Student> findByFullTime(boolean fullTime);
-    List<Student> findByAge(Integer age);
-    List<Student> findByAttendeeLastName(String last);
 
-    //Query Methods with Clauses and Exrpessions
-    Student findByAttendeeFirstNameAndAttendeeLastName(String firstName, String lastName);
-    Student findByAttendee(Person person);
-    List<Student> findByAgeGreaterThan(int minimumAge);
-    List<Student> findByAgeLessThan(int maximumAge);
-    List<Student> findByAttendeeLastNameIgnoreCase(String lastName);
-    List<Student> findByAttendeeLastNameLike(String likeString);
-    Student findFirstByOrderByAttendeeLastNameAsc();
-    Student findTopByOrderByAgeDesc();
-    List<Student> findTop3ByOrderByAgeDesc();
 }
