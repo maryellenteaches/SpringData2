492e25d2c813aeb54a41b97ac3a9aaacf4a344c8
MiellyEllen <maryellen.bowman@gmail.com>
2019-08-03 14:42:37 -0400

    reactive changes

--- university/src/main/java/com/example/university/MongoDbApplication.java
+++ university/src/main/java/com/example/university/MongoDbApplication.java
@@ -1,11 +1,5 @@
 package com.example.university;
 
-import com.example.university.domain.Department;
-import com.example.university.domain.Person;
-import com.example.university.domain.Staff;
-import com.example.university.repo.DepartmentRepository;
-import com.example.university.repo.StaffRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,36 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class MongoDbApplication implements CommandLineRunner  {
-    @Autowired
-    StaffRepository staffRepository;
-
-    @Autowired
-    DepartmentRepository departmentRepository;
-
-
     public static void main(String[] args) {
         SpringApplication.run(MongoDbApplication.class, args);
     }
 
     @Override
     public void run(String... strings) throws Exception {
-        //Staff
-        Staff deanJones = staffRepository.save(new Staff(1, new Person("John", "Jones"))).block();
-        Staff deanMartin = staffRepository.save(new Staff(2, new Person("Matthew", "Martin"))).block();
-        Staff profBrown = staffRepository.save(new Staff(3, new Person("James", "Brown"))).block();
-        Staff profMiller = staffRepository.save(new Staff(4, new Person("Judy", "Miller"))).block();
-        Staff profDavis = staffRepository.save(new Staff(5, new Person("James", "Davis"))).block();
-        Staff profMoore = staffRepository.save(new Staff(6, new Person("Allison", "Moore"))).block();
-        Staff profThomas = staffRepository.save(new Staff(7, new Person("Tom", "Thomas"))).block();
-        Staff profGreen = staffRepository.save(new Staff(8, new Person("Graham", "Green"))).block();
-        Staff profWhite = staffRepository.save(new Staff(9, new Person("Whitney", "White"))).block();
-        Staff profBlack = staffRepository.save(new Staff(10, new Person("Jack", "Black"))).block();
-        Staff profKing = staffRepository.save(new Staff(11, new Person("Queen", "King"))).block();
-
-        //Departments
-        Department humanities = departmentRepository.save(new Department(100, "Humanities", deanJones)).block();
-        Department naturalSciences = departmentRepository.save(new Department(200, "Natural Sciences", deanMartin)).block();
-        Department socialSciences = departmentRepository.save(new Department(300, "Social Sciences", deanJones)).block();
-
     }
 }
--- university/src/test/java/com/example/university/MongoDbDemos.java
+++ university/src/test/java/com/example/university/MongoDbDemos.java
@@ -1,5 +1,8 @@
 package com.example.university;
 
+import com.example.university.domain.Department;
+import com.example.university.domain.Person;
+import com.example.university.domain.Staff;
 import com.example.university.repo.DepartmentRepository;
 import com.example.university.repo.StaffRepository;
 import org.junit.After;
@@ -9,6 +12,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Demonstrate Various Querying Techniques with Spring Data MongoDb
@@ -25,6 +34,9 @@ public class MongoDbDemos {
 
 	@Autowired
     DepartmentRepository departmentRepository;
+
+  //  static ;;
+    static List<Staff> allStaff = new ArrayList();
      /**
      * Queries to Mongo DB.
      *
@@ -33,54 +45,36 @@ public class MongoDbDemos {
      */
 	@Test
 	public void mongoQueryMethods() {
-//
-//	    Department d = departmentRepository.findAll().doOnEach(departmentSignal -> {
-//	        System.out.println(departmentSignal.get());
-//        });
-//        //***************Staff query methods***************
+        //Staff
+        Flux<Staff> staffFlux = staffRepository.findAll();
+        System.out.println(staffFlux.blockLast());
 
-//        //Paging and Sorting Queries
-//        System.out.println("\nFind all staff members, sort alphabetically by last name");
-//        Sort sortByLastName = new Sort(Sort.Direction.ASC, "member.lastName");
-//        Flux<Staff> flux = staffRepository.findAll(sortByLastName)
-//                .doFinally(System.out::println(s));
-//
-//        System.out.println("\nFind first 5 Staff members, sort alphabetically by last name");
-//        Page<Staff> members = staffRepository.findAll(PageRequest.of(0, 5, sortByLastName));
-//        members.forEach(System.out::println);
-//
-//
-//        //Property Expression
-//        System.out.println("\nFind all staff members with last name King");
-//        staffRepository.findByMemberLastName("King").forEach(System.out::println);
-//
-//        //@Query with JSON query string
-//        //"{ 'member.firstName' : ?0 }"
-//        System.out.println("\nFind all staff members with first name John");
-//        staffRepository.findByFirstName("John").forEach(System.out::println);
-//
-//
-//        //***************Department query methods***************
-//
-//        //Sorting example, MongoRepository extends PagingAndSortingRepository
-//        System.out.println("\nFind all Departments, sort alphabetically by  name");
-//        departmentRepository.findAll(new Sort(Sort.Direction.ASC, "name")).forEach(System.out::println);
-//
-//        //Property Expression
-//        System.out.println("\nFind Department with the exact name 'Humanities' \n" +
-//                departmentRepository.findByName("Humanities"));
-//
-//        //@Query with JSON query string that accepts regular expression as a parameter
-//        //{ 'name' : { $regex: ?0 } }
-//        //Any department name that ends in sciences where 's' is case insensitive
-//        System.out.println("\nFind all Departments with name ending in Sciences");
-//        departmentRepository.findNameByPattern(".[Ss]ciences").forEach(System.out::println);
-//
-//
-//        //Invalid Method, will fail at runtime
-//        System.out.println("\nInvalid Method, cannot cross DBRef's in queries");
-//        departmentRepository.findByChairMemberLastName("Jones");
+        Mono<Staff> deanJonesMono = staffRepository.save(new Staff(1, new Person("John", "Jones")));
+        Mono<Staff> deanMartinMono = staffRepository.save(new Staff(2, new Person("John", "Martin")));
+
+        System.out.println("Before Staff has subscribed " + staffFlux.count().block());
+
+        Staff deanJones = deanJonesMono.block();
+        deanMartinMono.subscribe();
+        Staff deanMartin = staffRepository.findByMemberLastName("Martin").blockFirst();
+
+        System.out.println(("After Staff has subscribed " + staffFlux.count().block()));
+
+        //Departments
+        departmentRepository.saveAll(
+                Arrays.asList(new Department(100, "Humanities", deanJones),
+                            new Department(200, "Natural Sciences", deanMartin),
+                            new Department(300, "Social Sciences", deanJones)))
+                .blockLast();
+
+        System.out.println(("Total Departments " + departmentRepository.findAll().count().block()));
+	}
+
+	private void print(String message, Flux<?> flux) {
+
+	    System.out.println(message + ":\t " + flux.count().block());
     }
+
     @Before
     @After
     public void printBanner() {
