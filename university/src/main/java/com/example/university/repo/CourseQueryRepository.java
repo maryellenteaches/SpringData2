1daef25fee97641d54e2a03b4a39ba0b2af255af
MiellyEllen <maryellen.bowman@gmail.com>
2019-08-04 16:18:36 -0400

    CourseQueryRepository

--- university/src/main/java/com/example/university/repo/CourseQueryRepository.java
+++ university/src/main/java/com/example/university/repo/CourseQueryRepository.java
@@ -3,7 +3,7 @@ package com.example.university.repo;
 import com.example.university.domain.Course;
 
 /**
- * Declaring a CouseQueryRepository which can only query the Database
+ * Declaring a CourseQueryRepository which can only query the Database
  */
 public interface CourseQueryRepository extends ReadOnlyRepository<Course, Integer> {
 }
