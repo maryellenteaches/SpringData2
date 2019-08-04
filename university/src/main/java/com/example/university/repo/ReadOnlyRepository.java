58b5d5a05de22aae7b41a6fe25a2807edd34e269
MiellyEllen <maryellen.bowman@gmail.com>
2019-08-04 16:04:18 -0400

    update read only repository

--- university/src/main/java/com/example/university/repo/ReadOnlyRepository.java
+++ university/src/main/java/com/example/university/repo/ReadOnlyRepository.java
@@ -3,12 +3,10 @@ package com.example.university.repo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -19,17 +17,18 @@ import java.util.Optional;
 @NoRepositoryBean
 public interface ReadOnlyRepository<T, ID extends Serializable> extends Repository<T,ID > {
 
-    T findOne(ID id);
 
-    boolean exists(ID id);
+    Optional<T> findById(ID id);
 
     Iterable<T> findAll();
 
-    Iterable<T> findAll(Sort sort);
+    Iterable<T> findAllById(Iterable<ID> iterable);
 
-    Iterable<T> findAll(Iterable<ID> iterable);
+    Iterable<T> findAll(Sort sort);
 
     Page<T> findAll(Pageable pageable);
 
     long count();
+
+    boolean existsById(ID id);
 }
