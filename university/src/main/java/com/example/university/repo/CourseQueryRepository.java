7af12d66831c8fdae0fea63946ea75d06a98929f
MiellyEllen <maryellen.bowman@gmail.com>
2019-08-07 10:53:09 -0400

    Updated exerciese to remove ReadOnlyRepo

--- university/src/main/java/com/example/university/repo/CourseQueryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.university.repo;
-
-import com.example.university.domain.Course;
-
-/**
- * Declaring a CourseQueryRepository which can only query the Database
- */
-public interface CourseQueryRepository extends ReadOnlyRepository<Course, Integer> {
-}
